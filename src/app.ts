52f8ec7b034f51603c622b06607432db97dabf3c
diegoberselli <diegoberselliz@gmail.com>
2022-09-12 21:35:13 -0300

    add:healthcheck

--- src/app.ts
+++ src/app.ts
@@ -25,3 +25,12 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
 
   return res.status(500).json({ message: "Internal server error" });
 });
+
+app.get("/", (req: Request, res: Response) => {
+  const healthcheck = {
+    uptime: process.uptime(),
+    message: "Ok, running",
+    timestamp: Date.now(),
+  };
+  res.send(healthcheck);
+});
