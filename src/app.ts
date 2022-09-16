f64a6dda265a80c55f5aebedf3a10c2cbc840a9c
diegoberselli <diegoberselliz@gmail.com>
2022-09-16 15:14:58 -0300

    add:swagger

--- src/app.ts
+++ src/app.ts
@@ -18,8 +18,8 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.use("/api-docs", swaggerUi.serve);
-app.get("/api-docs", swaggerUi.setup(swaggerDocs));
+// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof AppError) {
